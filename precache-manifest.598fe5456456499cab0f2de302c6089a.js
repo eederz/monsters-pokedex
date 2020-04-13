a8ef84047701e604f169d3e328ca243bfd15891d
Eduardo  Baños <ebanos@Eduardos-MacBook-Pro.local>
2020-04-13 10:10:20 -0500

    Updates

---
+++ precache-manifest.598fe5456456499cab0f2de302c6089a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c0bace6bae21ab6cf2b91e51889f3fb",
+    "url": "/monsters-pokedex/index.html"
+  },
+  {
+    "revision": "f6c339f4a7c8d9dc1d51",
+    "url": "/monsters-pokedex/static/css/main.3006ea93.chunk.css"
+  },
+  {
+    "revision": "bd4da6ab1d9e405c24fb",
+    "url": "/monsters-pokedex/static/js/2.e552c0ac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-pokedex/static/js/2.e552c0ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6c339f4a7c8d9dc1d51",
+    "url": "/monsters-pokedex/static/js/main.36bf1f3c.chunk.js"
+  },
+  {
+    "revision": "87326a0d5a90b35084aa",
+    "url": "/monsters-pokedex/static/js/runtime-main.614614f6.js"
+  }
+]);
